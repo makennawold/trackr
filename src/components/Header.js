c46fa70b6845631d98822c2a376c8462322d1e22
makennawold <makennawold@gmail.com>
2021-11-20 15:25:57 -0600

    added button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
   //destructuring props, makes it so we dont have to do props.
+
+  const onClick = () => {
+    console.log("click");
+  };
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="add" onClick={onClick} />
     </header>
   );
 };
@@ -16,5 +22,6 @@ Header.defaultProps = {
 Header.propTypes = {
   title: PropTypes.string,
 };
+// can do .isRequired and make sure that prop is being passed down, whch will only error if you don't have a default props
 
 export default Header;
